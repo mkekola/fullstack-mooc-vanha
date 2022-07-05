dee78d5fd946a18c89a2c74807feb74f3935f1c1
Maria <maria.kekola@helsinki.fi>
2022-07-05 17:21:49 +0300

    osa1 kurssitiedot

---
+++ osa_1/kurssitiedot/src/App.js
@@ -0,0 +1,57 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
+  };
+
+  const Part = (props) => {
+    return (
+      <>
+        <p>
+          {props.part.name} {props.part.exercises}
+        </p>
+      </>
+    );
+  };
+
+  const Content = (props) => {
+    return props.parts.map((part) => {
+      return <Part part={part} />;
+    });
+  };
+
+  const Total = (props) => {
+    let total = 0;
+    props.parts.forEach((part) => {
+      total += part.exercises;
+    });
+    return <p>Number of exercises {total}</p>;
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
